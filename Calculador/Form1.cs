fcd46390df2862ce68c45b8e3778f65cdea325ec
Maria0610 <maria.cruz74@unach.mx>
2025-03-25 00:36:54 -0600

    Cambios

--- Calculador/Form1.cs
+++ Calculador/Form1.cs
@@ -1,8 +1,8 @@
 namespace Calculador
 {
-    public partial class Form1 : Form
+    public partial class Calculadora : Form
     {
-        public Form1()
+        public Calculadora()
         {
             InitializeComponent();
         }
